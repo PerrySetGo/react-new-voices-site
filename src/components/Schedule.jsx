5feafa7724c3d6303c753fa13f5f4f418eb7c74f
Perry Eising <perry.r.eising@gmail.com>
2018-05-28 12:56:57 -0700

    JSX looping - issue with key on first loop

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,41 +2,66 @@ import React from 'react'
 import ScheduleHeaderItemSort from './ScheduleHeaderItemSort'
 import Event from './Event'
 
+var masterScheduleHeaderItemList = [
+  {title:'Day'},
+  {title:'Time'},
+  {title:'Locations'},
+  {title:'Track'},
+]
+
+
+var masterEventList = [
+  {
+    day:'12/12/2019',
+    time:'10am-11am',
+    location:'green room',
+    track:'data track',
+    title:'props vs. state in react',
+    attendance:'true'
+  },
+  {
+    day:'12/12/2019',
+    time:'10am-11am',
+    location:'red roomyyy',
+    track:'UI track',
+    title:'CSS objects in react',
+    attendance:'false'
+  },
+  {
+    day:'12/13/2019',
+    time:'12pm-2pm',
+    location:'yellow room',
+    track:'People Skills',
+    title:'Salary Negotiation',
+    attendance:'true'
+  }
+]
+
 function Schedule(){
   return(
-    <table>
-      <th><ScheduleHeaderItemSort
-        title="Day"
-      /></th>
-      <th><ScheduleHeaderItemSort
-        title="Time"
-      /></th>
-      <th><ScheduleHeaderItemSort
-        title="Location"
-      /></th>
-      <th><ScheduleHeaderItemSort
-        title="Track"
-      /></th>
-      <th>Title</th>
-      <Event
-        day="12/12/2019"
-        time="10am-11am"
-        location="green room"
-        track="data track"
-        title="props vs. state in react"
-        attendance="true"
-      />
+    //table header row contains properties that will be sortable at some point - EXCEPT title, which is why it is hardcoded
 
-      <Event
-        day="12/12/2019"
-        time="10am-11am"
-        location="red room"
-        track="UI track"
-        title="CSS objects in react"
-        attendance="false"
-      />
+    <table>
+      {masterScheduleHeaderItemList.map((scheduleHeaderItemSort, index) =>
+        <th>
+          <ScheduleHeaderItemSort
+            title = {scheduleHeaderItemSort.title}
+            key = {index}
+          /></th>
+      )}
+      <th>Title</th>
+      {masterEventList.map((event, index) =>
+        <Event
+          day={event.day}
+          time={event.time}
+          location={event.location}
+          track={event.track}
+          title={event.title}
+          attendance={event.attendance}
+          key={index}
+        />
+      )}
     </table>
-  )
-}
+  )}
 
 export default Schedule
--- src/components/ScheduleHeaderItemSort.jsx
+++ src/components/ScheduleHeaderItemSort.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+//I have this long funny name as I will be how the schedule can be sorted when I grow up.
+
 function ScheduleHeaderItemSort(props) {
   return (
     <h3>{props.title} &and;&or;</h3>
