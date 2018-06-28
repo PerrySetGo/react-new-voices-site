903cae8aaf20e97538f8b692b552b85dc0396c41
perry eising <perry.r.eising@gmail.com>
2018-06-28 16:36:56 -0700

    add passing test - event is successfully added to master list

--- __tests__/reducers/event-list-reducer.test.js
+++ __tests__/reducers/event-list-reducer.test.js
@@ -2,7 +2,42 @@ import eventListReducer from './../../src/reducers/event-list-reducer';
 
 describe ('eventListReducer', () => {
 
+  let action;
+
+  const sampleEventData = {
+    date: '12/12/2017',
+    time: '10:10:30 am',
+    location: 'Green Room',
+    track: 'ui track',
+    title: 'ui testing workshop',
+    id:0
+  }
+
   test('Should return default state if no action type is recognized', () => {
-    expect(eventListReducer({}, {type: null })).toEqual({}); 
+    expect(eventListReducer({}, {type: null })).toEqual({});
+  });
+
+  test ('Should succcessfully add new event to masterEventList', () =>{
+    const {date, time, location, track, title, id} = sampleEventData;
+    action = {
+      type: 'ADD_EVENT',
+      date: date,
+      time: time,
+      location:location,
+      track: track,
+      title: title,
+      id: id
+    };
+
+    expect(eventListReducer({}, action)).toEqual({
+      [id] : {
+        date: date,
+        time: time,
+        location: location,
+        track: track,
+        title: title,
+        id: id
+      }
+    });
   });
 });
--- src/reducers/event-list-reducer.js
+++ src/reducers/event-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-
+  switch (action.type) {
+  case 'ADD_EVENT':
+  const {date, time, location, track, title, id} = action;
+    let newState = Object.assign({}, state, {
+      [id]: {
+        date:date,
+        time:time,
+        location:location,
+        track:track,
+        title:title,
+        id:id
+      }
+    });
+    return newState;
+  default:
+    return state;
+  }
 };
