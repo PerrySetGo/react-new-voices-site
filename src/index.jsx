4487481099e92f49b5a9801f524bed225e093552
perry eising <perry.r.eising@gmail.com>
2018-06-28 17:17:07 -0700

    add react-redux support

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import { AppContainer } from 'react-hot-loader'
 import { HashRouter } from 'react-router-dom'
+import { createStore } from 'redux';
+import eventListReducer from './reducers/event-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(eventListReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
       <HashRouter>
-        <Component/>
-      </HashRouter>  
-    </AppContainer>,
+        <Provider store={store}>
+          <Component/>
+        </Provider>
+      </HashRouter>
     document.getElementById('react-app-root')
   )
 }
