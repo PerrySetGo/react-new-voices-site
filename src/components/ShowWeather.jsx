1ca0b95db10ea311307b0a27766c3442c0d692f8
perry eising <perry.r.eising@gmail.com>
2018-07-08 16:47:56 -0700

    add state to ShowWeather component

--- src/components/ShowWeather.jsx
+++ src/components/ShowWeather.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { initialState } from './../../src/constants/InitialState';
 
-function ShowWeather(){
+class ShowWeather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
   //run the action to parse the weather
   //connect to redux or not??
   //how to dispatch the action.
-
-  return(
-    <div>
-    'I am the show weather component'
-    </div>
-  );
+  render(){
+    return(
+      <div>
+      'I am the show weather component'
+      </div>
+    );
+  }
 }
 
 export default ShowWeather;
